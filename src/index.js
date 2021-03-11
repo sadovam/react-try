c3e739668556201a0528e8e4c3f16c36edad22c2
Sadov Andrii <sadovam@gmail.com>
2021-03-11 12:26:06 +0200

    First react element

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+//   React.createElement(
+//     String/ReactClass type,
+//     [object props],
+//     [children...]
+//   ) -> React Element
+
+const element = React.createElement('h1', {}, 'Hello React Element');
+
+//   ReactDOM.render(
+//     ReactElement element,
+//     DOMElement container,
+//     [function callback]
+//   ) -> ReactComponent
+
+const root = document.getElementById('root');
+
+// console.log(root);
+// console.log(element);
+
 ReactDOM.render(
-  <h1>Hello World</h1>,
-  document.getElementById('root')
+  element,
+  root
 );
